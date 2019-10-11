2ba7228bddb86677ffc2a006b1d74ea1a4aa7296
leumumas <samuellalibel@hotmail.com>
2019-10-11 19:51:20 -0400

    first pass earth

--- LesPtitsGits3D/Assets/LowPolyEarth/EarthSpinScript.cs
+++ LesPtitsGits3D/Assets/LowPolyEarth/EarthSpinScript.cs
@@ -1,13 +1,62 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
+using System.Collections.Generic;
 
 public class EarthSpinScript : MonoBehaviour
 {
     public float speed = 10f;
+    public float speedZoom = 10f;
+    public float CloseValueCamera = -1.5f;
+    public float FarValueCamera = -5f;
+
+    private Camera MainCamera;
+
+    private float StartTime;
+    private float journeyLength = 0f;
+    private Vector3 StartPosition;
+    private Vector3 EndPosition;
+
+    private Vector3 FarPosition;
+    private Vector3 ClosePosition;
+
+    private void Start()
+    {
+        MainCamera = Camera.main;
+        FarPosition = new Vector3(0, 0, FarValueCamera);
+        ClosePosition = new Vector3(0, 0, CloseValueCamera);
+        StartPosition = FarPosition;
+        EndPosition = FarPosition;
+        StartTime = Time.time;
+        MainCamera.transform.position = EndPosition;
+    }
 
     void Update() 
     {
-        //transform.Rotate(Vector3.up, speed * Time.deltaTime, Space.World);
+        if (Input.GetButtonDown("ZoomIn"))
+        {
+            StartTime = Time.time;
+            journeyLength = Math.Abs(MainCamera.transform.position.z - CloseValueCamera);
+            StartPosition = MainCamera.transform.position;
+            EndPosition = ClosePosition;
+        }
+
+        if (Input.GetButtonDown("ZoomOut"))
+        {
+            StartTime = Time.time;
+            journeyLength = Math.Abs(MainCamera.transform.position.z - FarValueCamera);
+            StartPosition = MainCamera.transform.position;
+            EndPosition = FarPosition;
+        }
+        
+        float distCovered = (Time.time - StartTime) * speedZoom;
+        
+        float fractionOfJourney = distCovered / journeyLength;
+
+        if (fractionOfJourney < 1f)
+        {
+            MainCamera.transform.position = Vector3.Lerp(StartPosition, EndPosition, fractionOfJourney);
+        }
 
         float translation = -Input.GetAxis("Vertical") * speed;
         float rotation = Input.GetAxis("Horizontal") * speed;
