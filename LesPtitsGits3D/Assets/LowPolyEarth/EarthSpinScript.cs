9d2858aede1a1415b79efe31910cfcb378492962
leumumas <samuellalibel@hotmail.com>
2019-10-11 19:00:10 -0400

    Ajout stylist earth et début move

---
+++ LesPtitsGits3D/Assets/LowPolyEarth/EarthSpinScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EarthSpinScript : MonoBehaviour
+{
+    public float speed = 10f;
+
+    void Update() 
+    {
+        //transform.Rotate(Vector3.up, speed * Time.deltaTime, Space.World);
+
+        float translation = -Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * speed;
+
+        transform.Rotate(Vector3.up, rotation * Time.deltaTime, Space.World);
+        transform.Rotate(Vector3.right, translation * Time.deltaTime, Space.World);
+    }
+}
