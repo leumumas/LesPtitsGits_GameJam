47bbfae0ba754e66576d24a7fc55af7e273c2c01
leumumas <samuellalibel@hotmail.com>
2019-10-12 10:11:50 -0400

    cahnges

--- LesPtitsGits3D/Assets/Scripts/Earth/GlobalRegion.cs
+++ LesPtitsGits3D/Assets/Scripts/Earth/GlobalRegion.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class GlobalRegion : MonoBehaviour
 {
+    [SerializeField]
+    private string m_Name;
+    [SerializeField]
+    private int m_Population;
+
     // Start is called before the first frame update
     void Start()
     {
