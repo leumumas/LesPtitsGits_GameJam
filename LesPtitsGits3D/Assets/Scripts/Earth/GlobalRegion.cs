699ab5277b9f6b11a5e3152a7dabd4e739b117d4
leumumas <samuellalibel@hotmail.com>
2019-10-12 13:17:24 -0400

    Region Data added

--- LesPtitsGits3D/Assets/Scripts/Earth/GlobalRegion.cs
+++ LesPtitsGits3D/Assets/Scripts/Earth/GlobalRegion.cs
@@ -5,19 +5,13 @@ using UnityEngine;
 public class GlobalRegion : MonoBehaviour
 {
     [SerializeField]
-    private string m_NameRegion;
-    [SerializeField]
-    private long m_Population;
+    private RegionData m_CurrentRegion;
     [SerializeField]
     private bool m_Ocean = false;
 
-    public long Population => m_Population;
-    public string NameRegion => m_NameRegion;
+    public long Population => m_CurrentRegion.m_Population;
+    public string NameRegion => m_CurrentRegion.m_NameRegion;
     public bool Ocean => m_Ocean;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
 }
