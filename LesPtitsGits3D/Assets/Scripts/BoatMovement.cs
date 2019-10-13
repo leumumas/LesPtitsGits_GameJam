98b86b44d327703380f32f2036364a4960272d93
Dejays <davidtardif47@gmail.com>
2019-10-13 14:44:59 -0400

    Added boat pathing, Planes are now flying in the correct rotation

--- LesPtitsGits3D/Assets/Scripts/BoatMovement.cs
+++ LesPtitsGits3D/Assets/Scripts/BoatMovement.cs
@@ -4,29 +4,52 @@ using UnityEngine;
 
 public class BoatMovement : MonoBehaviour
 {
-    private Vector3 endPosition;
+    private GameObject[] path;
     private Vector3 direction;
     private Transform myTransform;
+    private int currentPathIndex = 0;
+
+    private Vector3 lookAt;
+    private Transform pathTransform;
 
     // Start is called before the first frame update
     void Start()
     {
         myTransform = GetComponent<Transform>();
+        pathTransform = path[currentPathIndex].GetComponent<Transform>();
+        lookAt = direction.normalized;
+
+        Quaternion targetRotation = Quaternion.LookRotation(lookAt, myTransform.position - new Vector3(0, 0, 0));
+        myTransform.rotation = targetRotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        myTransform.Translate(direction * Time.deltaTime * 20f, Space.World);
-        if((endPosition - myTransform.position).magnitude < 1)
+        myTransform.Translate(lookAt * Time.deltaTime * 20f, Space.World);
+
+        if((pathTransform.position - myTransform.position).magnitude < 2)
         {
-            Destroy(gameObject);
-        }
+            currentPathIndex++;
+
+            if (currentPathIndex == path.Length)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                pathTransform = path[currentPathIndex].GetComponent<Transform>();
+                lookAt = pathTransform.position - myTransform.position;
+                Quaternion targetRotation = Quaternion.LookRotation(lookAt, myTransform.position - new Vector3(0,0,0));
+                lookAt = lookAt.normalized;
+                myTransform.rotation = targetRotation;
+            }
+        }       
     }
 
-    public void SetEndPosition(Vector3 direction, Vector3 endPosition)
+    public void SetPath(Vector3 direction, GameObject[] pathToUse)
     {
-        this.endPosition = endPosition;
+        this.path = pathToUse;
         this.direction = direction.normalized;
     }
 }
---
+++ LesPtitsGits3D/Assets/Scripts/PortPaths.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortPaths : MonoBehaviour
+{
+    public PathsArray[] AllPaths;
+    private System.Random rnd = new System.Random();
+    public GameObject[] GetPath()
+    {
+        GameObject[] pathToTake = AllPaths[rnd.Next(0, AllPaths.Length)].Path;
+        return pathToTake;
+    }
+}
+
+[System.Serializable]
+public class PathsArray
+{
+    public GameObject[] Path;
+}
